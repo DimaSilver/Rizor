d26d60ee65f2bb3219f26429621498511ac4b988
DimaSilver <dimka7600@ukr.net>
2020-11-27 23:53:00 +0300

    Застилизовал сэкшен benefits.color-choice. Табы

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function () {
-    ('body').slick({});
-})
+$(function (){
+    $('.tab').on('click', function (e) {
+        e.preventDefault();
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).closest('.tabs-wrapper').siblings().find('a')).removeClass('tabs-content--active');
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+    });
+});
